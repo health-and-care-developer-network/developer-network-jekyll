bbee5188c5e7b17a2f35ecd72fe334b7b7cc1ed5
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2016-09-26 16:27:44 +0100

    Troubleshoot build

--- compilePages.sh
+++ compilePages.sh
@@ -14,13 +14,15 @@ echo "DN URL: $DEVNET_URL"
 echo "Breadcrumb: $BREADCRUMB"
 
 # Generate HTML
-rm -Rf /docker-data/generated/*
-docker run --net=host -v /docker-data/generated:/home/generator/output nhsd/jekyllpublish sh -c "/generate.sh $GITHUB_URL $DEVNET_URL $BREADCRUMB"
+#rm -Rf /docker-data/generated/*
+#docker run --net=host -v /docker-data/generated:/home/generator/output nhsd/jekyllpublish sh -c "/generate.sh $GITHUB_URL $DEVNET_URL $BREADCRUMB"
 
 # Now, build an nginx container to serve up the pages
+echo "WHOAMI?:"
+whoami
 mkdir nginx/site
 cp -R /docker-data/generated/* nginx/site
 docker build --no-cache -t $OUTPUT_IMAGE_NAME nginx/.
-rm -Rf /docker-data/generated/*
+#rm -Rf /docker-data/generated/*
 #rm -Rf ./nginx/site
 
