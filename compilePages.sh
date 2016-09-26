6064bf2e39328f4dc26c907ac304d66d85a96ae7
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2016-09-26 16:54:11 +0100

    Troubleshoot build

--- compilePages.sh
+++ compilePages.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Usage compilePages.sh github_url devnet_url output_image_name
+# Usage compilePages.sh github_url devnet_url output_image_name context_path
 
 GITHUB_URL=$1
 DEVNET_URL=$2
 OUTPUT_IMAGE_NAME=$3
 BREADCRUMB=$4
-
-echo "Running compile script"
-echo "Parameters:"
-echo "GIT URL: $GITHUB_URL"
-echo "DN URL: $DEVNET_URL"
-echo "Breadcrumb: $BREADCRUMB"
+CONTEXT_PATH=$5
 
 # Generate HTML
 docker run --net=host -v /docker-data/generated:/home/generator/output nhsd/jekyllpublish sh -c "/generate.sh $GITHUB_URL $DEVNET_URL $BREADCRUMB"
 
 # Now, build an nginx container to serve up the pages
-mkdir nginx/site
-cp -R /docker-data/generated/* nginx/site
+mkdir -p nginx/site/$CONTEXT_PATH
+cp -R /docker-data/generated/* nginx/site/%CONTEXT_PATH
 docker build --no-cache -t $OUTPUT_IMAGE_NAME nginx/.
 rm -Rf /docker-data/generated/*
 rm -Rf ./nginx/site
