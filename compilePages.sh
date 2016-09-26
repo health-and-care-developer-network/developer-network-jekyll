0cb65cac2d7c5ab3ff0be17d4b903cf0f102112e
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2016-09-26 09:48:26 +0100

    Fixed build script

--- compilePages.sh
+++ compilePages.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Usage compilePages.sh github_url devnet_url output_container_name
+# Usage compilePages.sh github_url devnet_url output_image_name
 
 GITHUB_URL=$1
 DEVNET_URL=$2
-OUTPUT_CONTAINER_NAME=$3
+OUTPUT_IMAGE_NAME=$3
 
 # Generate HTML
 mkdir /tmp/site
 chmod 777 /tmp/site
-docker run --net=host -v /tmp/site:/output nhsd/jekyllpublish sh -c '/generate.sh GITHUB_URL DEVNET_URL'
+docker run --net=host -v /tmp/site:/output nhsd/jekyllpublish sh -c '/generate.sh $GITHUB_URL $DEVNET_URL'
 
 # Now, build an nginx container to serve up the pages
 mv /tmp/site nginx/site
-docker build --no-cache -t $OUTPUT_CONTAINER_NAME nginx/.
+docker build --no-cache -t $OUTPUT_IMAGE_NAME nginx/.
 rm -Rf nginx/site
+
