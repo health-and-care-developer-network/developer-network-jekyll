3a27b1384f4e484d2e8f75247ce43cb610739908
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2016-09-27 13:13:06 +0100

    Updated styles in build

--- generate/generate.sh
+++ generate/generate.sh
@@ -28,7 +28,9 @@ sed -i '/NHS Digital Logo/c\<!--  -->' _includes/sidebar.html
 sed -i '/a\[href\^="http:\/\/"\]:after/c\\.apicontent > a\[href\^="http:\/\/"\]:after, a\[href\^="https:\/\/"\]:after {' css/customstyles.css
 echo ".search_box #s { height: 33px; }" >> css/customstyles.css
 echo ".main_nav .wrapper ul li a { height: 54px; }" >> css/customstyles.css
-
+echo ".page_title hgroup { height: 160px; }" >> css/customstyles.css
+echo ".page_title h1 { margin-top: 0px; }" >> css/customstyles.css
+echo ".page_title h2 { margin-top: 0px; padding-top: 0px; }" >> css/customstyles.css
 
 # Move the default page template to a different name
 mv _layouts/default.html _layouts/default-old.html
